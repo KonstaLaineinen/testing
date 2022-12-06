7d3a19feb421bc22fba9b08b47e334d1688573e2
KonstaLaineinen <konsta.laineinen@windowslive.com>
2022-12-06 19:23:49 +0200

    Added to repo

---
+++ src/CarMain.java
@@ -0,0 +1,19 @@
+public class CarMain {
+    public CarMain() {
+    }
+
+    public static void main(String[] args) {
+        carClass car1 = new carClass();
+        carClass car2 = new carClass("Ferrari");
+        car1.brand = "Fiat";
+        car1.model = "500";
+        car1.amountOfFuel = 10;
+        car1.printData();
+        car1.accelerate();
+        car1.brake();
+        car1.printData();
+
+
+
+    }
+}
---
+++ src/carClass.java
@@ -0,0 +1,64 @@
+public class carClass {
+
+    //Attributes
+    public String brand;
+    public String model;
+    public int amountOfFuel;
+    public int refuel = 5;
+
+
+    //Default constructor
+
+    public carClass(){
+        brand="";
+        model="";
+
+
+    }
+
+    public carClass(String brandName){
+        brand=brandName;
+        model="458";
+        amountOfFuel = 30;
+
+    }
+
+
+    //Methods
+
+    public void brake () {
+        System.out.println("Car is braking");
+    }
+
+
+    public void accelerate () {
+        System.out.println("Car is accelerating");
+        amountOfFuel = amountOfFuel - 5;
+        if (amountOfFuel == 0) {
+            System.out.println("No petrol");
+            System.exit (0);
+        }
+    }
+
+
+    public void printData () {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Fuel in tank: " + amountOfFuel);
+
+    }
+    public void refuel (int amount) {
+        System.out.println("Fuel in tank: " + amountOfFuel);
+        amountOfFuel = amountOfFuel + refuel;
+        System.out.println("Refuel: 5");
+        System.out.println("Fuel in tank after refill: " + amountOfFuel);
+    }
+
+
+
+
+
+}
+
+
+
